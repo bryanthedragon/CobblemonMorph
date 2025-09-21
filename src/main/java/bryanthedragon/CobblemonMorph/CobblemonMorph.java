d0006892f7455543636067c964e4d44d9daaa515
bryanthedragon <107201903+bryanthedragon@users.noreply.github.com>
2025-09-21 00:30:21 -0500

    BASE

--- src/main/java/bryanthedragon/CobblemonMorph/CobblemonMorph.java
+++ src/main/java/bryanthedragon/CobblemonMorph/CobblemonMorph.java
@@ -5,16 +5,6 @@ import com.mojang.logging.LogUtils;
 import net.minecraft.client.Minecraft;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.Item;
-//import net.minecraft.core.registries.Registries;
-//import net.minecraft.world.level.block.Block;
-//import net.minecraft.world.level.block.Blocks;
-//import net.minecraft.world.level.block.state.BlockBehaviour;
-//import net.minecraft.world.level.material.MapColor;
-//import net.minecraft.world.food.FoodProperties;
-//import net.minecraft.world.item.BlockItem;
-//import net.minecraft.world.item.CreativeModeTab;
-//import net.minecraft.world.item.CreativeModeTabs;
-
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
@@ -26,9 +16,6 @@ import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.ForgeRegistries;
-//import net.minecraftforge.registries.DeferredRegister;
-//import net.minecraftforge.registries.RegistryObject;
-//import net.minecraftforge.fml.config.ModConfig;
 import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
