e630894415ee9f7cfee0ac6b37571d793b5a340f
bryanthedragon <107201903+bryanthedragon@users.noreply.github.com>
2025-09-21 22:10:48 -0500

    BASE

---
+++ src/main/java/bryanthedragon/morph/cobblemonmorph/utils/mon/gen/eight/Gen8MonUtils.java
@@ -0,0 +1,106 @@
+package bryanthedragon.morph.cobblemonmorph.utils.mon.gen.eight;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import bryanthedragon.morph.cobblemonmorph.utils.mon.MonUtils;
+
+public class Gen8MonUtils extends MonUtils 
+{
+    private static List<String> validMonNames = new ArrayList<>();
+
+    public static void Gen8MonUtil() 
+    {
+        validMonNames.add("Grookey");
+        validMonNames.add("Thwackey");
+        validMonNames.add("Rillaboom");
+        validMonNames.add("Scorbunny");
+        validMonNames.add("Raboot");
+        validMonNames.add("Cinderace");
+        validMonNames.add("Sobble");
+        validMonNames.add("Drizzile");
+        validMonNames.add("Inteleon");
+        validMonNames.add("Skwovet");
+        validMonNames.add("Greedent");
+        validMonNames.add("Rookidee");
+        validMonNames.add("Corvisquire");
+        validMonNames.add("Corviknight");
+        validMonNames.add("Blipbug");
+        validMonNames.add("Dottler");
+        validMonNames.add("Orbeetle");
+        validMonNames.add("Nickit");
+        validMonNames.add("Thievul");
+        validMonNames.add("Gossifleur");
+        validMonNames.add("Eldegoss");
+        validMonNames.add("Wooloo");
+        validMonNames.add("Dubwool");
+        validMonNames.add("Chewtle");
+        validMonNames.add("Drednaw");
+        validMonNames.add("Yamper");
+        validMonNames.add("Boltund");
+        validMonNames.add("Rolycoly");
+        validMonNames.add("Carkol");
+        validMonNames.add("Coalossal");
+        validMonNames.add("Applin");
+        validMonNames.add("Flapple");
+        validMonNames.add("Appletun");
+        validMonNames.add("Silicobra");
+        validMonNames.add("Sandaconda");
+        validMonNames.add("Cramorant");
+        validMonNames.add("Arrokuda");
+        validMonNames.add("Barraskewda");
+        validMonNames.add("Toxel");
+        validMonNames.add("Toxtricity");
+        validMonNames.add("Sizzlipede");
+        validMonNames.add("Centiskorch");
+        validMonNames.add("Clobbopus");
+        validMonNames.add("Grapploct");
+        validMonNames.add("Scraggy");
+        validMonNames.add("Scrafty");
+        validMonNames.add("Sigilyph");
+        validMonNames.add("Yamask");
+        validMonNames.add("Runerigus");
+        validMonNames.add("Milcery");
+        validMonNames.add("Alcremie");
+        validMonNames.add("Falinks");
+        validMonNames.add("Pincurchin");
+        validMonNames.add("Snom");
+        validMonNames.add("Frosmoth");
+        validMonNames.add("Stonjourner");
+        validMonNames.add("Eiscue");
+        validMonNames.add("Indeedee");
+        validMonNames.add("Morpeko");
+        validMonNames.add("Cufant");
+        validMonNames.add("Copperajah");
+        validMonNames.add("Dracozolt");
+        validMonNames.add("Arctozolt");
+        validMonNames.add("Dracovish");
+        validMonNames.add("Arctovish");
+        validMonNames.add("Duraludon");
+        validMonNames.add("Dreepy");
+        validMonNames.add("Drakloak");
+        validMonNames.add("Dragapult");
+        validMonNames.add("Zacian");
+        validMonNames.add("Zamazenta");
+        validMonNames.add("Eternatus");
+        validMonNames.add("Kubfu");
+        validMonNames.add("Urshifu");
+        validMonNames.add("Zarude");
+        validMonNames.add("Regieleki");
+        validMonNames.add("Regidrago");
+        validMonNames.add("Glastrier");
+        validMonNames.add("Spectrier");
+        validMonNames.add("Calyrex");
+        validMonNames.add("Wyrdeer");
+        validMonNames.add("Kleavor");
+        validMonNames.add("Ursaluna");
+        validMonNames.add("Basculegion");
+        validMonNames.add("Sneasler");
+        validMonNames.add("Overqwil");
+        validMonNames.add("Enamorus");
+    }
+    public static void getGen8MonUtils() 
+    {
+        Gen8MonUtil();
+    }
+}
---
+++ src/main/java/bryanthedragon/morph/cobblemonmorph/utils/mon/gen/six/Gen6MonUtils.java
@@ -0,0 +1,88 @@
+package bryanthedragon.morph.cobblemonmorph.utils.mon.gen.six;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import bryanthedragon.morph.cobblemonmorph.utils.mon.MonUtils;
+
+public class Gen6MonUtils extends MonUtils
+{
+    private static List<String> validMonNames = new ArrayList<>();
+    private static void Gen6MonUtil()
+    {
+        validMonNames.add("Chesnaught");
+        validMonNames.add("Fennekin");
+        validMonNames.add("Braixen");
+        validMonNames.add("Delphox");
+        validMonNames.add("Froakie");
+        validMonNames.add("Frogadier");
+        validMonNames.add("Greninja");
+        validMonNames.add("Bunnelby");
+        validMonNames.add("Diggersby");
+        validMonNames.add("Fletchling");
+        validMonNames.add("Fletchinder");
+        validMonNames.add("Talonflame");
+        validMonNames.add("Scatterbug");
+        validMonNames.add("Spewpa");
+        validMonNames.add("Vivillon");
+        validMonNames.add("Litleo");
+        validMonNames.add("Pyroar");
+        validMonNames.add("Flabebe");
+        validMonNames.add("Floette");
+        validMonNames.add("Florges");
+        validMonNames.add("Skiddo");
+        validMonNames.add("Gogoat");
+        validMonNames.add("Pancham");
+        validMonNames.add("Pangoro");
+        validMonNames.add("Furfrou");
+        validMonNames.add("Espurr");
+        validMonNames.add("Meowstic");
+        validMonNames.add("Honedge");
+        validMonNames.add("Doublade");
+        validMonNames.add("Aegislash");
+        validMonNames.add("Spritzee");
+        validMonNames.add("Aromatisse");
+        validMonNames.add("Swirlix");
+        validMonNames.add("Slurpuff");
+        validMonNames.add("Inkay");
+        validMonNames.add("Malamar");
+        validMonNames.add("Binacle");
+        validMonNames.add("Barbaracle");
+        validMonNames.add("Skrelp");
+        validMonNames.add("Dragalge");
+        validMonNames.add("Clauncher");
+        validMonNames.add("Clawitzer");
+        validMonNames.add("Helioptile");
+        validMonNames.add("Heliolisk");
+        validMonNames.add("Tyrunt");
+        validMonNames.add("Tyrantrum");
+        validMonNames.add("Amaura");
+        validMonNames.add("Aurorus");
+        validMonNames.add("Sylveon");
+        validMonNames.add("Hawlucha");
+        validMonNames.add("Dedenne");
+        validMonNames.add("Carbink");
+        validMonNames.add("Goomy");
+        validMonNames.add("Sliggoo");
+        validMonNames.add("Goodra");
+        validMonNames.add("Klefki");
+        validMonNames.add("Phantump");
+        validMonNames.add("Trevenant"); 
+        validMonNames.add("Pumpkaboo"); 
+        validMonNames.add("Gourgeist");
+        validMonNames.add("Bergmite");
+        validMonNames.add("Avalugg");
+        validMonNames.add("Noibat");
+        validMonNames.add("Noivern");
+        validMonNames.add("Xerneas");
+        validMonNames.add("Yveltal");
+        validMonNames.add("Zygarde");
+        validMonNames.add("Diancie");
+        validMonNames.add("Hoopa");
+        validMonNames.add("Volcanion");
+    }
+    public static void getGen6MonUtils() 
+    {
+        Gen6MonUtil();
+    }
+}
